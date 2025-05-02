10661827716431b19ef0316831c5ffafdd1f1f53
Djuri Baars <dsbaars@users.noreply.github.com>
2025-05-02 23:44:50 +0200

    feat: Switch to kraken websocket v2

--- server/price-sources/kraken-ws.ts
+++ server/price-sources/kraken-ws.ts
@@ -3,41 +3,41 @@ import { WsPriceSource } from "./ws-price-source";
 
 export class KrakenPriceSource extends WsPriceSource {
   constructor(pair:string) {
-    const ws = new WsConnection('wss://ws.kraken.com');
+    const ws = new WsConnection('wss://ws.kraken.com/v2');
     ws.on('open', () => {
       const subscribeMessage = {
-        event: 'subscribe',
-        pair: [
-          'BTC/USD',  // For BTC to USD
-          'BTC/EUR',  // For BTC to EUR
-          'BTC/GBP',  // For BTC to GBP
-          'BTC/CAD',  // For BTC to CAD
-          'BTC/CHF',  // For BTC to CHF
-          'BTC/AUD',  // For BTC to AUD
-          'BTC/JPY'   // For BTC to JPY
-        ],
-        subscription: {
-          name: 'ticker'
+        method: 'subscribe',
+        params: {
+          channel: 'ticker',
+          symbol: [
+            'BTC/USD',  // For BTC to USD
+            'BTC/EUR',  // For BTC to EUR
+            'BTC/GBP',  // For BTC to GBP
+            'BTC/CAD',  // For BTC to CAD
+            'BTC/CHF',  // For BTC to CHF
+            'BTC/AUD',  // For BTC to AUD
+            'BTC/JPY'   // For BTC to JPY
+          ],
         }
       };
 
       ws.send(JSON.stringify(subscribeMessage));
-      console.log(`Kraken: Subscribed to ${subscribeMessage.pair.join(',')} ticker`);
+      console.log(`Kraken: Subscribed to ${subscribeMessage.params.symbol.join(',')} ticker`);
     });
 
     ws.on('message', (data) => {
       const message = JSON.parse(data.toString());
-
+      // console.log(message);
       // Handle subscription status
-      if (message.event === 'subscriptionStatus' && message.status === 'subscribed') {
-        //console.log(`Subscription status: ${message.status} to ${message.pair}`);
+      if (message.method === 'subscribe' && message.success === true) {
+        console.log(`Subscription status: ${message.result.channel} to ${message.result.symbol}`);
       }
 
       // Handle ticker data
-      if (Array.isArray(message) && message[2] === 'ticker') {
-        const tickerData = message[1];
-        //console.log(`${message[3]} Price: ${tickerData.c[0]}`);
-        this.emit('priceUpdate', { source: 'kraken', pair: String(message[3]).substring(4), price: tickerData.c[0] } );
+      if (Array.isArray(message.data) && message.channel === 'ticker') {
+        const tickerData = message.data[0];
+        // console.log(`${tickerData.symbol} Price: ${tickerData.last} Pair: ${String(tickerData.symbol).substring(4)}`);
+        this.emit('priceUpdate', { source: 'kraken', pair: String(tickerData.symbol).substring(4), price: tickerData.last } );
       }
     });
 
